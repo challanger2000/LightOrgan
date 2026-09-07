cacbc163464d3dbc71a558cbcffb62804f206a54
challanger2000 <challanger2000@gmx.de>
2026-09-07 12:05:41 +0200

    Use dedicated band-pass analyzers

--- src/LightOrganProcessor.h
+++ src/LightOrganProcessor.h
@@ -12,8 +12,7 @@ static const ParamID kSensitivityId = 102;
 static const ParamID kDecayId = 103;
 static const ParamID kBrightnessId = 104;
 static const ParamID kStrobeThresholdId = 105;
-
-static const ParamID kLampBaseId = 200; // 200..205
+static const ParamID kLampBaseId = 200;
 static const ParamID kStrobeMeterId = 206;
 
 static const FUID LightOrganProcessorUID(0x6A4F1001, 0x4C8E41A2, 0xB3D93417, 0x7A10C001);
@@ -30,24 +29,33 @@ public:
     static FUnknown* createInstance(void*) { return static_cast<IAudioProcessor*>(new LightOrganProcessor()); }
 
 private:
+    struct BandFilter {
+        double b0=0.0,b1=0.0,b2=0.0,a1=0.0,a2=0.0;
+        double z1=0.0,z2=0.0;
+        double process(double x) {
+            const double y=b0*x+z1;
+            z1=b1*x-a1*y+z2;
+            z2=b2*x-a2*y;
+            return y;
+        }
+        void reset(){z1=z2=0.0;}
+    };
+
     void updateParameters(ProcessData&);
     void resetAnalysis();
     void updateCoefficients();
     void publishMeter(ProcessData&, ParamID, ParamValue);
-
     template <typename Sample>
     void passAndAnalyze(ProcessData&, Sample** in, Sample** out, int32 channels);
 
     double sampleRate_ = 44100.0;
-    std::array<double, 5> lp_{};
-    std::array<double, 5> coeff_{};
-    std::array<double, 6> lampEnv_{};
+    std::array<BandFilter,6> bands_{};
+    std::array<double,6> lampEnv_{};
     double slowPeak_ = 0.0;
     double strobeEnv_ = 0.0;
     double strobeCooldownSeconds_ = 0.0;
-
     bool power_ = true;
-    int mode_ = 1; // 0 Organ, 1 Both, 2 Strobe
+    int mode_ = 1;
     double sensitivity_ = 0.55;
     double decay_ = 0.42;
     double brightness_ = 0.85;
