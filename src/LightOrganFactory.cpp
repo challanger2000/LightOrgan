437275ca668bcc6f34e6ccfaa7e13ad1f05ed0ad
challanger2000 <challanger2000@gmx.de>
2026-09-07 08:35:39 +0200

    Add LightOrgan plugin factory

---
+++ src/LightOrganFactory.cpp
@@ -0,0 +1,29 @@
+#include "LightOrganProcessor.h"
+#include "public.sdk/source/main/pluginfactory.h"
+
+using namespace Steinberg;
+using namespace Steinberg::Vst;
+
+BEGIN_FACTORY_DEF("challanger2000", "https://github.com/challanger2000/", "mailto:example@example.com")
+
+DEF_CLASS2(INLINE_UID_FROM_FUID(LightOrganProcessorUID),
+           PClassInfo::kManyInstances,
+           kVstAudioEffectClass,
+           "LightOrgan",
+           Vst::kDistributable,
+           Vst::PlugType::kFx,
+           "0.1.0",
+           kVstVersionString,
+           LightOrganProcessor::createInstance)
+
+DEF_CLASS2(INLINE_UID_FROM_FUID(LightOrganControllerUID),
+           PClassInfo::kManyInstances,
+           kVstComponentControllerClass,
+           "LightOrganController",
+           0,
+           "",
+           "0.1.0",
+           kVstVersionString,
+           LightOrganController::createInstance)
+
+END_FACTORY
