0888ad423089c23eb589c504a6d0a0710e4e2343
challanger2000 <challanger2000@gmx.de>
2026-09-07 11:48:23 +0200

    Add custom LightOrgan editor scaffold

---
+++ src/LightOrganEditor.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "vstgui/plugin-bindings/vst3editor.h"
+
+namespace Steinberg::Vst {
+
+class LightOrganEditor final : public VSTGUI::VST3Editor {
+public:
+    explicit LightOrganEditor(EditController* controller);
+    VSTGUI::CView* createView(const VSTGUI::UIAttributes& attributes,
+                              const VSTGUI::IUIDescription* description) override;
+
+private:
+    EditController* controller_ = nullptr;
+};
+
+} // namespace Steinberg::Vst
